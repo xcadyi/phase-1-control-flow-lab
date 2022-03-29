754cc050809fa4167d67fda2cc10004a15f97ba1
Selin Ayyildiz <xcadyix@gmail.com>
2022-03-29 16:04:43 -0400

    commit tree 2660c987bb79e5c2acdee6f661e1cb3453628165

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!"
+  } else if (feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (feet > 2500) ; {
+    return "No can do."
+  }}
+ 
+function ternaryCheckCity(city) {
+   return city === 'NYC'?'Ok, sounds good.':'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous' :
+      return 'Thank you so much.'
+        case 'not as generous' :
+          return 'Thank you.'
+            default:
+              return 'Bye.' 
+  }}
